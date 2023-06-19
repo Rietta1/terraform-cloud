a44b6b09e42167db2f408e1c5c8a73a114178ffd
Rietta1 <riettabusiness@gmail.com>
2023-06-19 20:08:05 +0100

    changes to desired capacity & min_size

--- main.tf
+++ main.tf
@@ -72,8 +72,8 @@ module "autoscaling" {
   ami-web           = var.ami-web
   ami-bastion       = var.ami-bastion
   ami-nginx         = var.ami-nginx
-  desired_capacity  = 2
-  min_size          = 2
+  desired_capacity  = 1
+  min_size          = 1
   max_size          = 2
   web-sg            = [module.security.web-sg]
   bastion-sg        = [module.security.bastion-sg]
